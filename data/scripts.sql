0d2b14131a069713e6c780bdaf45af64efde77c7
Casey Smith <crs4f1988@gmail.com>
2022-08-27 11:04:49 -0500

    4 finally done

--- data/scripts.sql
+++ data/scripts.sql
@@ -23,3 +23,24 @@ ON p.playerid = m.playerid
 WHERE schoolname = 'Vanderbilt University'
 GROUP BY s.schoolname, c.playerid, p.namefirst, p.namelast
 ORDER BY SUM(m.salary) DESC;
+
+Select POS, playerid
+CASE WHEN POS = 'OF' THEN 'Outfield'
+WHEN POS = 'SS' THEN 'Infield'
+WHEN POS = '1B' THEN 'Infield'
+WHEN POS = '2B' THEN 'Infield'
+WHEN POS = '3B' THEN 'Infield'
+WHEN POS = 'P' THEN 'Battery'
+WHEN POS = 'C' THEN 'Battery'
+ELSE END AS 'Other'
+FROM fielding;
+
+SELECT SUM(po) AS pos_putouts, 
+CASE WHEN pos = 'OF' THEN 'Outfield'
+WHEN pos IN ('SS', '1B', '2B', '3B') THEN 'Infield'
+WHEN pos IN ('P', 'C') THEN 'Battery'
+ELSE 'Other'
+END AS position
+FROM fielding
+WHERE yearid = '2016'
+GROUP BY position;
